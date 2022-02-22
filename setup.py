8e8f878584e06d614f664bed4f04352bcdabbd75
santiagxf <fasantia@microsoft.com>
2022-02-22 10:55:30 -0500

    fix: dictionaries and lists

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jobtools",
-    version="0.0.9",
+    version="0.0.10",
     author="Facundo Santiago",
     description="Facilitates the use of Python from the command line",
     url = 'https://github.com/santiagxf/jobtools',
